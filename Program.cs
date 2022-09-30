774a5b6f93e660cb3cae80ffcd0e05fa8113be42
Vadim Pudov <pvb@rambler.ru>
2022-09-30 22:49:18 +0700

    with changes

--- Program.cs
+++ Program.cs
@@ -1,53 +1,35 @@
-﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
 
 Console.Clear();
-Console.WriteLine("программа, создающая массив из 8 введённых в консоль чисел\nВам необходимо ввести 8 целых чисел");
+Console.WriteLine("программа, создающая массив из N введённых в консоль чисел\nВам необходимо ввести N целых чисел");
 
-string str = "";
-int[] array = new int[8];
+int count = 1;
+List<int> array = new List<int>();
 
-for (int count = 0; count < 8; count++)
-{
-    while(true)
-    {
-        Console.WriteLine("Введите " + Count(count) + " число:");
-
-        if(int.TryParse(Console.ReadLine(), out int number))
-        {
-            array[count] = number;
+while(true) {
+    Console.WriteLine("Введите " + count + "-е число:");
+        string str = Console.ReadLine();
+        if(str == "стоп") {
+            break;
         }
-        else 
-        {
+        if(int.TryParse(str, out int number)) {
+            array.Add(number);
+            count++;
+            continue;
+        }
+        else {
             Console.WriteLine("Некорректно указано число!\n");
             continue;
         }
-    break;
     }
-}
 
 Console.WriteLine("Вы создали массив: "); Print(array);
 
-//////////////////////////////////////////////////////////////////
-string Count(int count)
+void Print(List<int> array)
 {
-    if(count == 0) str = "первое";
-    if(count == 1) str = "второе";
-    if(count == 2) str = "третье";
-    if(count == 3) str = "четвёртое";
-    if(count == 4) str = "пятое";
-    if(count == 5) str = "шестое";
-    if(count == 6) str = "седьмое";
-    if(count == 7) str = "восьмое";
-    
-    return str;
-}
-
-void Print(int[] array)
-{
-    for(int index = 0; index < 8; index++)
-    {
+    for(int index = 0; index < count - 1; index++) {
         if(index == 0) Console.Write("[" + array[index] + ", ");
-        else if (index == 7) Console.Write(array[index] + "]!");
+        else if (index == count - 2) Console.Write(array[index] + "]!");
         else Console.Write(array[index] + ", ");
     }
 }
